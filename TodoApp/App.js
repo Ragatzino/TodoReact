24df7a4a39ccf7c4ec32364ae862c5f18fae7a17
MailineN <nguyen.mailine@gmail.com>
2021-03-15 18:02:47 +0100

    Added list

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import{StyleSheet,Text,View,SafeAreaView,TouchableOpacity} from "react-native"; 
+import{StyleSheet,Text,View,SafeAreaView,TouchableOpacity,FlatList} from "react-native"; 
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+// import itemList from '../components/listItem'
 
 export default class App extends React.Component {
   render () {
@@ -18,9 +19,20 @@ export default class App extends React.Component {
             <TouchableOpacity style = {styles.add}>
             <MaterialCommunityIcons name="cat" size={45} color="#6495ed"/>
             </TouchableOpacity>
-            <Text style={styles.addText}> Miaou </Text>
+            <Text style={styles.addText}> Miaou! </Text>
+           
           </View>
+          <View style = {{height : 500}}>
+          <Text style={styles.addText}> Choses à faire :</Text>
+            <FlatList data = {data} 
+              keyExtractor = {item => item.text}
+              renderItem= {({item}) => (
+                <View style ={[styles.listContainer]}>
+                  <Text style = {styles.listText}>{item.id}. <Text style ={{fontWeight : "300"}}> {item.text} </Text>
+                  </Text>
+                </View>)} />
 
+          </View>
         </SafeAreaView>
       </View>
     );
@@ -37,7 +49,7 @@ const styles = StyleSheet.create(
     }, 
     divider: {
       backgroundColor: "#000000", 
-      height:1,
+      height:10,
       flex:1,
       alignSelf: "center"
     },
@@ -60,5 +72,41 @@ const styles = StyleSheet.create(
       paddingVertical : 10,
     } ,
 
+    listText :{
+      fontSize : 22,
+      fontWeight: "500", 
+      color : "#ffff", 
+      alignSelf: "center", 
+      paddingVertical : 10,
+    } ,
+
+    listContainer: {
+      paddingVertical : 10, 
+      paddingHorizontal : 10, 
+      borderRadius : 40, 
+      alignItems: "center", 
+      width : 500,
+      backgroundColor : "#6495ed", 
+      marginVertical: 10
+    }
   }
 );
+
+const data = [
+  {
+    id : 1,
+    text: "Dire à Zachou d'arreter friends",
+  },
+  {
+    id : 2,
+    text : "Et Chihiro",
+  },
+  {
+    id : 3,
+    text : "Arreter de vouloir rusher le cours",
+  },
+  {
+    id : 4,
+    text: "Je vais dodo",
+  },
+];
